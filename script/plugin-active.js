d8fd350e01c7650af4b9dbd443608be86914cc4d
Gopinath <gopinath@Gopinath-MBP.local>
2018-05-16 13:59:19 +0530

    alignment issue fixed

--- script/plugin-active.js
+++ script/plugin-active.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-	// banner slider 
+  // banner slider 
   var owl = $('#BannerSlider').owlCarousel({
     loop: true,
     margin: 0,
     items: 1,
     dots: true,
-    autoHeight:true,
-    autoplay:true
+    autoHeight: true,
+    autoplay: true
   });
   // sticky header
   $(window).scroll(function() {
     if ($(this).scrollTop() > 1) {
-        $('body').addClass("sticky");
+      $('body').addClass("sticky");
     } else {
-        $('body').removeClass("sticky");
+      $('body').removeClass("sticky");
     }
   });
   // smooth scroll
-  $(".smooth-scroll").click(function() {        
+  $(".smooth-scroll").click(function() {
     var target = $(this).attr("href"),
-      scrollTo = $(target).offset().top,         
-      headerHeight=$('#Header').outerHeight();
-      console.log(headerHeight);
-      $('html, body').animate({ scrollTop : scrollTo - headerHeight});
+      scrollTo = $(target).offset().top,
+      headerHeight = $('#Header').outerHeight();
+    console.log(headerHeight);
+    $('html, body').animate({ scrollTop: scrollTo - headerHeight });
     $('#nav-icon1').trigger("click");
     $("body").removeClass("menu-active");
   });
   //menu icon animation
-  $('#nav-icon1').click(function(){
+  $('#nav-icon1').click(function() {
     $(this).toggleClass('open');
     $("body").toggleClass("menu-active");
   });
@@ -42,19 +42,19 @@ $(document).ready(function() {
   });
   // contact form script
   $('.form-wrap input, .form-wrap textarea, .email-box input').blur(function() {
-      tmpval = $(this).val();
-      if (tmpval == '') {
-          $(this).addClass('empty');
-          $(this).removeClass('not-empty');
-      } else {
-          $(this).addClass('not-empty');
-          $(this).removeClass('empty');
-      } 
+    tmpval = $(this).val();
+    if (tmpval == '') {
+      $(this).addClass('empty');
+      $(this).removeClass('not-empty');
+    } else {
+      $(this).addClass('not-empty');
+      $(this).removeClass('empty');
+    }
   });
   // Our Work filter
   $('#OurWork .filter-list ul li').click(function() {
     dis = $(this),
-        disFilter = dis.data("filter");
+      disFilter = dis.data("filter");
     finalFilter = disFilter == "all" ? "*" : "." + disFilter;
     dis.addClass('active').siblings().removeClass('active');
     $("#OurWork .filter-box .each-box").hide().filter(finalFilter).show();
